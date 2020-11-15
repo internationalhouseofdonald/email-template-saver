8170d8eabe72dea58d327d7468bac1f224dc7851
internationalhouseofdonald <donaldwaynemoorejr@gmail.com>
2020-11-15 14:52:58 -0600

    Setting up Mongo.db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from "../../util/mongodb";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  const leads = await db.collection("leads").find({}).toArray();
+  console.log(leads);
+  res.statusCode = 200;
+  res.json({ name: "Jon Doe", leads });
+};
